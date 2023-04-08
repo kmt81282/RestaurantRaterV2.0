6f0c425196a3686de8358e29d0b8eddc2eb27ff3
marcturner <kturner54@student.gsu.edu>
2023-04-08 13:41:11 -0400

    Initial Commit

---
+++ RestaurantRaterV2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestaurantRaterV2.0
+//
+//  Created by Marc Turner on 4/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
